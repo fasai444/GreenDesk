0c09a4ce8f0bca749764bd37cd33a3f76412f9e6
Hadi ISSA <166885675+Hadiissa1@users.noreply.github.com>
2026-02-19 06:58:58 +0100

    Modifié  repositories.EffectRepository.java
    Ajout de findByIsCustom(boolean isCustom).

--- src/main/java/org/example/repositories/EffectRepository.java
+++ src/main/java/org/example/repositories/EffectRepository.java
@@ -4,9 +4,13 @@ import org.example.entites.Effect;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EffectRepository extends MongoRepository<Effect, String> {
     Optional<Effect> findByName(String name);
+    // CELUI QU'IL FAUT AJOUTER pour la Livraison 3
+    // Il permet de faire : "Donne-moi tous les effets qui sont custom (ou pas)"
+    List<Effect> findByIsCustom(boolean isCustom);
 }
